782d280a6561e818f76590a8dbf51f8ab0bfb2ea
JosiasSandoval <josisan0803@gmail.com>
2025-09-17 13:45:26 -0500

    avance_1

---
+++ static/js/header.js
@@ -0,0 +1,5 @@
+fetch('/static/templates/header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header-placeholder').innerHTML = data;
+            });
